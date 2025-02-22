36b5ff84a12e9673a972192b150b5228d0fada6b
John Ronnel <robledojohnronnel14@gmail.com>
2025-02-23 00:46:08 +0800

    Add Layouts using Template Inheretance

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Task List App</title>
+</head>
+
+<body>
+
+   <h1>@yield('title')</h1>
+    <div>
+        @yield('content') <!-- Section for Page Content -->
+    </div>
+
+
+</body>
+</html>
